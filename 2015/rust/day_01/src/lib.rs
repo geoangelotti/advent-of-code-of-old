9f0b2022ea1d70ee80d728f1fa9dcd7851a2631b
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-02-07 18:25:23 +0100

    refactor part 1

--- 2015/rust/day_01/src/lib.rs
+++ 2015/rust/day_01/src/lib.rs
@@ -1,11 +1,13 @@
+fn floor_resolve(c: char) -> i32 {
+    match c {
+        '(' => 1,
+        ')' => -1,
+        char => panic!("invalid input: {{{}}}", char),
+    }
+}
+
 pub fn process_part_1(input: &str) -> i32 {
-    input.chars().fold(0, |acc, c| {
-        acc + match c {
-            '(' => 1,
-            ')' => -1,
-            char => panic!("invalid input: {{{}}}", char),
-        }
-    })
+    input.chars().fold(0, |acc, c| acc + floor_resolve(c))
 }
 
 #[cfg(test)]
@@ -23,7 +25,7 @@ mod tests {
     #[case("))(", -1)]
     #[case(")))", -3)]
     #[case(")())())", -3)]
-    fn line_test(#[case] line: &str, #[case] expected: i32) {
+    fn test_part_1(#[case] line: &str, #[case] expected: i32) {
         let result = process_part_1(line);
         assert_eq!(result, expected);
     }
