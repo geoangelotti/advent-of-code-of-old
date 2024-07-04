50738f13b56a16de39fd3257c9a5e5fbb74fd727
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-07-05 00:40:13 +0200

    tests

--- 2015/rust/day_06/src/lib.rs
+++ 2015/rust/day_06/src/lib.rs
@@ -105,14 +105,24 @@ mod tests {
     use super::*;
     use rstest::rstest;
 
-    const COMMANDS: &str = "turn on 0,0 through 999,999
+    const COMMANDS_1: &str = "turn on 0,0 through 999,999
 toggle 0,0 through 999,0
 turn off 499,499 through 500,500";
 
+    const COMMANDS_2: &str = "turn on 0,0 through 0,0
+toggle 0,0 through 999,999";
+
     #[rstest]
-    #[case(COMMANDS, 998996)]
+    #[case(COMMANDS_1, 998996)]
     fn test_procces_part_1(#[case] input: &str, #[case] expected: u32) {
         let result = process_part_1(input);
+        assert_eq!(result, expected);
+    }
+
+    #[rstest]
+    #[case(COMMANDS_2, 2000001)]
+    fn test_procces_part_2(#[case] input: &str, #[case] expected: u32) {
+        let result = process_part_2(input);
         assert_eq!(result, expected);
     }
 }
