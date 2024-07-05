633107e2d128c98de61778148245abd642151540
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-07-05 21:28:02 +0200

    gate emulation

--- 2015/rust/day_07/src/lib.rs
+++ 2015/rust/day_07/src/lib.rs
@@ -1,5 +1,53 @@
 use std::collections::BTreeMap;
 
+#[derive(Clone, Copy, Debug)]
+enum InputPort<'a> {
+    Port(&'a str),
+    Value(u16),
+}
+
+impl<'a> InputPort<'a> {
+    fn resolve(&self, map: &BTreeMap<&str, u16>) -> u16 {
+        match &self {
+            InputPort::Port(key) => *map.get(key).unwrap(),
+            InputPort::Value(v) => *v,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Gate {
+    ARROW,
+    AND,
+    OR,
+    NOT,
+    LSHIFT,
+    RSHIFT,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Instruction<'a> {
+    gate: Gate,
+    input: [InputPort<'a>; 2],
+    output: &'a str,
+}
+
+impl<'a> Instruction<'a> {
+    fn execute(&self, map: &mut BTreeMap<&'a str, u16>) {
+        let a = self.input[0].resolve(map);
+        let b = self.input[1].resolve(map);
+        let c = match &self.gate {
+            Gate::ARROW => a,
+            Gate::AND => a & b,
+            Gate::OR => a | b,
+            Gate::NOT => !a,
+            Gate::LSHIFT => a << b,
+            Gate::RSHIFT => a >> b,
+        };
+        map.insert(self.output, c);
+    }
+}
+
 fn process_part_1(input: &str) -> BTreeMap<&str, u16> {
     let mut map = BTreeMap::new();
     return map;
