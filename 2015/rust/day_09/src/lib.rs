6d4fdf107ecd44bee0c5c32ba756a274b7b10df4
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-07-14 00:02:45 +0200

    All in a Single Night parsing

--- 2015/rust/day_09/src/lib.rs
+++ 2015/rust/day_09/src/lib.rs
@@ -1,4 +1,28 @@
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, alpha1, newline},
+    multi::separated_list1,
+    sequence::{preceded, separated_pair},
+    IResult,
+};
+
+fn parse_locations(input: &str) -> IResult<&str, (&str, &str)> {
+    separated_pair(alpha1, tag(" to "), alpha1)(input)
+}
+
+fn parse_distance(input: &str) -> IResult<&str, (&str, &str, u32)> {
+    let (input, locations) = parse_locations(input)?;
+    let (input, distance) = preceded(tag(" = "), complete::u32)(input)?;
+    Ok((input, (locations.0, locations.1, distance)))
+}
+
+fn parse_distances(input: &str) -> IResult<&str, Vec<(&str, &str, u32)>> {
+    separated_list1(newline, parse_distance)(input)
+}
+
 pub fn process_part_1(input: &str) -> u32 {
+    let (_, distances) = parse_distances(input).unwrap();
+    dbg!(distances);
     todo!()
 }
 
