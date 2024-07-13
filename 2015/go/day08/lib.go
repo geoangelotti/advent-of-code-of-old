89e2ac3f5b88abff8b1ee1af04bbe27ff1bb2a84
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-07-13 18:52:29 +0200

    Matchsticks solution

--- 2015/go/day08/lib.go
+++ 2015/go/day08/lib.go
@@ -1,6 +1,26 @@
 package day08
 
-func ProcessPart1(input string) int {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-	return 0
+func unescape(input string) string {
+	unescaped, err := strconv.Unquote(input)
+	if err != nil {
+		return ""
+	}
+	return unescaped
+}
+
+func ProcessPart1(input string) int {
+	var acc int
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		unescaped := unescape(line)
+		fmt.Println(line, len(line), len(unescaped))
+		acc += len(line) - len(unescaped)
+	}
+	return acc
 }
